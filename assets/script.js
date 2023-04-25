379413225c3cc678fe02be339c3b73cb572794bb
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-04-25 19:10:24 -0300

    Add go_wasm

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const goWasm = new Go()
+
+WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
+    .then((result) => {
+        goWasm.run(result.instance)
+
+        document.getElementById("get-html").addEventListener("click", () => {
+            document.body.innerHTML += getHtml()
+        })
+    })
+
